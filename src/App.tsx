f6e3b789471217d5ca7c3704727c53407ec1e03a
Joxeph <codewithjoxeph@gmail.com>
2025-08-14 17:25:08 +0100

    initial file commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import gsap from "gsap"
+import { ScrollTrigger, SplitText } from "gsap/all"
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+export default function App() {
+  return (
+    <div className="flex-center w-full h-screen">
+    <div className="text-3xl">App
+
+    </div>
+    </div>
+  )
+}
